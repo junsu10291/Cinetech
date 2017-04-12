9132760606654cb81e450fb2bac7440f23eaefcc
junsu1029 <junsu_choi@brown.edu>
2017-04-12 11:39:24 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,107 @@
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "./views/main.html",
+    })
+    .when("/recommendation", {
+        templateUrl : "./views/recommendation.html"
+    })
+    .when("/account", {
+        templateUrl : "./views/account.html"
+    })
+    .when("/rate", {
+        templateUrl : "./views/rate.html"
+    })
+    .otherwise({redirectTo:'/'});
+});
+
+app.controller("appController", function($scope) {
+    $scope.movies = [];
+    $scope.stars = [];
+    $scope.modal_stars = [];
+    $scope.detail_movies = [];
+    $scope.modal_restart = false;
+
+     for (let i = 0; i < 15; i++) {
+        $scope.movies.push({"show": false});
+     }
+
+     for (let i = 0; i < 3; i++) {
+        $scope.detail_movies.push({"show": false});
+     }
+
+     for (let i = 0; i < 5; i++) {
+        $scope.stars.push({"id": i, "fill": false});
+     }
+
+     for (let i = 0; i < 5; i++) {
+        $scope.modal_stars.push({"id": i, "fill": false});
+     }
+
+    $scope.movieMouseOver = function(event) {
+        $scope.showPanel = true;
+    }
+
+    $scope.movieMouseLeave = function() {
+        $scope.showPanel = false;
+    }
+
+    $scope.focusStars = function(starId) {
+        for (let i = starId; i >= 0; i--) {
+            $scope.stars[i].fill = true;
+        }
+    }
+
+    $scope.unfocusStars = function() {
+        for (let i = 0; i < $scope.stars.length; i++) {
+            $scope.stars[i].fill = false;
+        }
+    }
+
+    $scope.focusModalStars = function(starId) {
+        for (let i = starId; i >= 0; i--) {
+            $scope.modal_stars[i].fill = true;
+        }
+    }
+
+    $scope.unfocusModalStars = function() {
+        for (let i = 0; i < $scope.stars.length; i++) {
+            $scope.modal_stars[i].fill = false;
+        }
+    }
+
+    $scope.detailToggleModal = function() {
+        $scope.modal_restart = true;
+        $('.movieDetailModal').modal('hide');    
+
+        $('.movieDetailModal').on('hidden.bs.modal', function (e) {
+            if ($scope.modal_restart) {
+                console.log("show!!");
+                $('.movieDetailModal').modal('show');
+                $scope.modal_restart = false;
+            }
+        });    
+    }
+
+    $scope.carouselLeft = function() {
+        $('#main-carousel').carousel('prev');
+    }
+
+    $scope.carouselRight = function() {
+        $('#main-carousel').carousel('next');
+    }
+
+    $scope.modalTrigger = function() {
+        $('.movieDetailModal').modal('toggle');
+    }
+
+    $scope.registerModalTrigger = function() {
+        $('#registerModal').modal('toggle');
+    }
+
+    $scope.loginModalTrigger = function() {
+        $('#loginModal').modal('toggle');
+    }
+});
